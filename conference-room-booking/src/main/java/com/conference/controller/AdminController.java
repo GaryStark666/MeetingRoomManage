c09375b800be016baa740470dae9b09bfded4e2d
小豆泥的编程之路 <148734780+huociyongbusuoxiwang@users.noreply.github.com>
2025-04-18 22:06:45 +0800

    111

---
+++ conference-room-booking/src/main/java/com/conference/controller/AdminController.java
@@ -0,0 +1,9 @@
+package com.conference.controller;
+
+import com.conference.service.AdminService;
+
+public class AdminController {
+
+    private AdminService adminService;
+
+}
---
+++ conference-room-booking/src/main/java/com/conference/entity/Admin.java
@@ -0,0 +1,4 @@
+package com.conference.entity;
+
+public class Admin {
+}
